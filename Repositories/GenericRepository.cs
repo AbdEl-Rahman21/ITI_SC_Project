2de03f5011d3b1794a4ffa8057b36b47fa0639e2
Abd El-Rahman Ali <abdel292004@gmail.com>
2025-10-09 14:16:20 +0300

    Add Generic repo

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using ITI_SC_Project.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ITI_SC_Project.Repositories
+{
+    public class GenericRepository<T>(HotelDbContext context) : IGenericRepository<T> where T : class
+    {
+        private readonly DbSet<T> dbSet = context.Set<T>();
+
+        public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter != null) query = query.Where(filter);
+
+            foreach (var include in includes) query = query.Include(include);
+
+            if (orderBy != null) query = orderBy(query);
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(object id) => await dbSet.FindAsync(id);
+
+        public async Task AddAsync(T entity) => await dbSet.AddAsync(entity);
+
+        public void Update(T entity) => dbSet.Update(entity);
+
+        public void Delete(T entity) => dbSet.Remove(entity);
+    }
+}
---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace ITI_SC_Project.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, params Expression<Func<T, object>>[] includes);
+
+        Task<T?> GetByIdAsync(object id);
+
+        Task AddAsync(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
